17c1bc82a75db2722d626d7c2d99bd76066a227e
BenLorenzoDev <benlorenzo.dev@gmail.com>
2025-11-18 19:48:02 +0800

    Update header to Tambayan PH with pink gradient theme

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,14 +11,13 @@ const Header = () => {
       transition={{ duration: 0.8 }}
     >
       <div className="header-content">
-        <motion.div 
+        <motion.div
           className="header-title"
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.8, delay: 0.2 }}
         >
-          <h1 className="main-title">Build with Vapi</h1>
-          <h2 className="sub-title">Challenge</h2>
+          <h1 className="main-title">Tambayan PH</h1>
         </motion.div>
         
         <motion.div 
